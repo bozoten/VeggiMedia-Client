9f3e2ed24b3eab7015bc79bc9a833a7fb90361fe
Pavelrh <shridharapavel@gmail.com>
2024-04-19 07:40:07 -0400

    crud done

--- src/app/services/media.service.ts
+++ src/app/services/media.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from '../environments/environments';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -20,4 +20,15 @@ export class MediaService {
   addMedia(media: any) {
     return this.http.post(`${this.serverUrl}/media`, media);
   }
+
+  // DELETE
+  deleteMedia(_id: string) {
+    return this.http.delete(`${this.serverUrl}/media/${_id}`);
+  }
+
+  // UPDATE using PUT
+  updateMedia(media: any) {
+    return this.http.put(`${this.serverUrl}/media/${media._id}`, media);
+  }
+
 }
