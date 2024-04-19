2fd4f6b965217ae1ee820e93dd51b66ec0feb490
Pavelrh <shridharapavel@gmail.com>
2024-04-19 05:16:02 -0400

    defined the class structure for mock data and exported the class objects as arrays

--- src/app/component/media/media.component.ts
+++ src/app/component/media/media.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+// define a class for the media structure
+export class Media {
+  _id: string | undefined;
+  title: string | undefined;
+  releaseYear: number | undefined;
+  type: string | undefined;
+  provider: string | undefined;
+  rating: number | undefined;
+}
+
 @Component({
   selector: 'app-media',
   standalone: true,
@@ -8,5 +18,9 @@ import { Component } from '@angular/core';
   styleUrl: './media.component.css'
 })
 export class MediaComponent {
-
+// mock data for this week, will come from API next week
+MEDIA: Media[] = [
+  {_id: 'abc123', title: 'Shogun', releaseYear: 2024, type: 'TV', provider: 'FX', rating: 5 },
+  {_id: 'def234', title: 'The Hunger Habit', releaseYear: 2023, type: 'Book', provider: 'Jud Brewer', rating: 5 },
+  {_id: 'ghi567', title: 'Bosch', releaseYear: 2018, type: 'TV', provider: 'Prime', rating: 4 }
 }
