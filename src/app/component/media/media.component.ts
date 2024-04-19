9cb570f5263219866288eb82abab7ba28220e599
Pavelrh <shridharapavel@gmail.com>
2024-04-19 05:18:30 -0400

    closed the array bracket while exporting

--- src/app/component/media/media.component.ts
+++ src/app/component/media/media.component.ts
@@ -24,4 +24,5 @@ MEDIA: Media[] = [
   {_id: 'abc123', title: 'Shogun', releaseYear: 2024, type: 'TV', provider: 'FX', rating: 5 },
   {_id: 'def234', title: 'The Hunger Habit', releaseYear: 2023, type: 'Book', provider: 'Jud Brewer', rating: 5 },
   {_id: 'ghi567', title: 'Bosch', releaseYear: 2018, type: 'TV', provider: 'Prime', rating: 4 }
+]
 }
